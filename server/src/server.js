98dfed58a61b6007b2591bb875f01d193fa0070c
Ruben Aguilar <ragui56@wgu.edu>
2026-06-17 00:28:19 -0700

    add database

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoute.js'
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5001
+
+connectDB()
 
 app.use('/api/notes', notesRoutes)
 
-const PORT = 5001
 app.listen(PORT, () => console.log(`Server running on port:${PORT}`))
