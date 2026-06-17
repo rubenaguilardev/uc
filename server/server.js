715d6b5ea888d4221aa3e7e8c97f6b3f39bef759
Ruben Aguilar <ragui56@wgu.edu>
2026-06-16 17:15:56 -0700

    feat add CRUD REST API endpoints for notes

--- server/server.js
+++ server/server.js
@@ -6,5 +6,17 @@ app.get('/api/notes', (req, res) => {
   res.send("You got 8 notes")
 })
 
+app.post('/api/notes', (req, res) => {
+  res.status(201).json({ message: "Note created successfully!" })
+})
+
+app.put('/api/notes/:id', (req, res) => {
+  res.status(200).json({ message: "Note updated successfully!" })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  res.status(200).json({ message: "Note deleted successfully!" })
+})
+
 const PORT = 5001
 app.listen(PORT, () => console.log(`Server running on port:${PORT}`))
